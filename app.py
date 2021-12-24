92cf0899351e9a882305ebb2d96a5976fff79dc0
iwakishinji <shinji071@gmail.com>
2021-12-25 02:32:30 +0900

    a

--- app.py
+++ app.py
@@ -13,14 +13,12 @@ def load_data():
     return df
 
 
-
-st.title('人生アルゴリズム')
 st.header('財布の小銭最小化問題')
-st.subheader('財布が軽くなるほど、心が満たされる。　　--ヴィクトル・ユーゴー')
-st.write('今回は支払い後の財布の中身の紙幣・硬貨枚数が最小になるアルゴリズムを考えてみました') # markdown
+st.subheader('財布が軽くなるほど、心が満たされる。　　¥n--ヴィクトル・ユーゴー')
+st.write('支払い後の財布の中身の紙幣・硬貨枚数を最小にする計算エンジンです！')
+st.write('財布の中身は左上の矢印を押すと表示される設定画面で設定できます。お買い上げ金額は↓で設定してください。最適計算ボタンを押すと、最適な支払い金額が算出されます！')
 
 st.sidebar.write('財布の中身(自由に設定してみてください）')
-
 yen10000 = st.sidebar.slider('10000円札', 0, 10, 1, 1)
 yen5000 = st.sidebar.slider('5000円札', 0, 10, 1, 1)
 yen2000 = st.sidebar.slider('2000円札', 0, 10, 1, 1)
@@ -32,11 +30,10 @@ yen10 = st.sidebar.slider('10円玉', 0, 10, 4, 1)
 yen5 = st.sidebar.slider('5円玉', 0, 10, 1, 1)
 yen1 = st.sidebar.slider('1円玉', 0, 10, 3, 1)
 
-st.write('財布の中身合計：', yen10000*10000+yen5000*5000+yen2000*2000+yen1000*1000+yen500*500+yen100*100+yen50*50+yen10*10+yen5*5+yen1*1)
-input_price = st.number_input('請求金額 : ', 1, 100000, 1200)
 
+input_price = st.number_input('請求金額 : ', 1, 1000000, 694)
+st.write('財布の中身合計(左上の矢印から変更可能)：', yen10000*10000+yen5000*5000+yen2000*2000+yen1000*1000+yen500*500+yen100*100+yen50*50+yen10*10+yen5*5+yen1*1)
 opt_payment = -1
-st.write("最適な支払い金額 = ", opt_payment)
 #st.button('最適計算')
 
 if st.button("最適計算"):
